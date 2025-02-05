acdfb6dd94d8562bead4cb55d2697a3f7b574430
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-02-05 16:44:13 +0600

    create js repo

---
+++ variable.js
@@ -0,0 +1,17 @@
+// variables keywords
+
+// var
+
+var name = "hurayra khan";
+console.log(name);
+
+// let
+let Name = "huraura khan";
+console.log(Name);
+
+// const
+
+const NAME = "hurayra khan";
+
+console.log(NAME);
+
