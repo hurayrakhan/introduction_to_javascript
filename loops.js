5f587efaa3b080bb722132da37f8e824376ddfdd
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-02-08 23:17:41 +0600

    learned loops

---
+++ loops.js
@@ -0,0 +1,17 @@
+// ************************************for loop********************************************
+
+const numbers = [11, 12, 13, 14, 15, 16, 17, 18];
+
+for (const number of numbers) {
+    console.log(number);
+}
+const fruits = ["mango", "orange", "banana", "apple"];
+for (const fruit of fruits) {
+    console.log("i want to eat");
+    console.log(fruit);
+    console.log("please give me");
+
+}
+
+// ************************************while loop*****************************************
+
