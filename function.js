f52e8d1b57df9db3d6e0eee443873f5979b29d98
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-02-13 14:46:57 +0600

    learned function

---
+++ function.js
@@ -0,0 +1,17 @@
+
+// structure of a function
+// function name(){}
+
+const number = 45;
+
+function fanOff() {
+    console.log("utho");
+    console.log("switch er kache jaw");
+    console.log("click the switch");
+      
+}
+// call the function
+fanOff();
+
+function sokaleBrushKoro(){}
+
