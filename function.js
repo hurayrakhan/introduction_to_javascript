7fea6bdc2b01a12c345770a29ecbb68dfbc96f5d
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-02-13 17:46:11 +0600

    learned more

--- function.js
+++ function.js
@@ -21,6 +21,18 @@ function doubleIt(number){
 doubleIt(15)
 
 
+// we can use a variable as the argument of a function
+// example
+function addition(num1, num2){
+    add = num1 + num2;
+    console.log(add);
+}
+const x = 20;
+const y = 12;
+// call function
+addition(x, y);
+
+// ******************return****************
 
 function sum(x, y){
     const result = x + y;
@@ -47,15 +59,56 @@ const result = fanOff();
 console.log(result);
 
 
-// we can use a variable as the argument of a function
-// example
-function addition(num1, num2){
-    add = num1 + num2;
-    console.log(add);
+// conditional return
+
+function isEven(number){
+    if (number % 2 === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-const x = 20;
-const y = 12;
-// call function
-addition(x, y);
+console.log(isEven(8));
+
+function isOdd(number){
+    if (number % 2 === 1){
+        return true;
+    }
+    return false;
+}
+console.log(isOdd(6));
+
+
+// string function
+
+function string(str){
+    const size = str.length;
+    console.log(str, size);
+
+
+}
+
+string("hurayra");
+
+
+// boolean function
+
+function doubleOrTriple(number, doDouble){
+    if(doDouble === true){
+        const double = number * 2;
+        return double
+    }
+    else{
+        const triple = number * 3;
+        return triple;
+    }
+}
+
+console.log(doubleOrTriple(5, true));
+console.log(doubleOrTriple(5, false));
+
+
+
 
 
