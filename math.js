66a324532907d62feb5ae2c3b8e9aee12f77ef7e
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-02-14 17:55:49 +0600

    math syntax

---
+++ math.js
@@ -0,0 +1,24 @@
+const min = Math.min(38, 78, 62, 12, 53);
+console.log(min);
+
+const max = Math.max(38, 78, 62, 12, 53);
+console.log(max);
+
+
+console.log(Math.PI);
+console.log(Math.abs(6-46));
+console.log(Math.round(4.54));
+console.log(Math.floor(4.53));
+console.log(Math.floor(4.13));
+console.log(Math.ceil(5.833));
+console.log(Math.ceil(5.133));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
+
+
+
+
