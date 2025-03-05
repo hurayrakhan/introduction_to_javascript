be0c360b6fe16f25f4c9aacde57fd2d899a60642
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-03-05 16:36:17 +0600

    learned spread_operator js

---
+++ spread_operator.js
@@ -0,0 +1,7 @@
+const friends = ["hurayra", "khan", "ali", "haque"];
+
+console.log(...friends);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// find the max number in the array
+console.log(Math.max(...numbers));
