0f19bfab65f64be4a7b0e0471b1a9074360571ef
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-03-06 16:46:12 +0600

    learned map method

---
+++ shrot_methodOf_loop/map.js
@@ -0,0 +1,11 @@
+// map function is a short form of loop[s]
+
+const numbers = [1, 2 , 3,4,5 ,6];
+
+const squre = numbers.map(element => element * element);
+const cube = numbers.map(element => {
+    return element * element * element;
+})
+
+console.log(squre);
+console.log(cube);
