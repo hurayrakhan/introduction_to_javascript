83fa743aa6f2327c4d3a138c1e3d617b1ee41d66
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-03-11 15:10:09 +0600

    learned getting data from server and use them in the website

--- api_json.js
+++ api_json.js
@@ -1,7 +1,15 @@
 
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+
+const getData = () => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => console.log(json))
+}
 
-
+const getUsers = () => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => console.log(users))
+}
