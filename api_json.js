53c4bcc13fa041f1acc4738c04d149ff2a18a34b
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-03-13 20:10:17 +0600

    self improved

--- api_json.js
+++ api_json.js
@@ -34,22 +34,25 @@ function persons (users){
 
 
 const handleFetch = async() =>{
-      const response = fetch("https://jsonplaceholder.typicode.com/users");
+      const response =await fetch("https://jsonplaceholder.typicode.com/users");
 
-      const data =await response.json();
+      const data = await response.json();
       console.log(data);
 
 }
+handleFetch()
 
 
-const handleFetchError =async () => {
+const handleFetchError = async () => {
       try{
-            const response = fetch("https://jsonplaceholder.typicode.com/users");
-            const data = (await response).json();
+            const response =await fetch("https://jsonplaceholder.typicode.com/users");
+            const data = await response.json();
             console.log(data);
       }
       catch{
-            console.log("Hello error",console.error()
-            )
+            console.log("Hello error")
       }
 }
+
+
+
